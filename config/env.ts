6665f6b746a2deebca392e9569847fc8f1aa8689
Rowyda020 <rowydaramadan@gmail.com>
2026-02-07 18:28:23 +0200

    fix: build fixes

---
+++ config/env.ts
@@ -0,0 +1,9 @@
+// Replace with your actual backend URL
+export const API_URL ='http://44.223.17.44:3003';
+export const GOOGLE_WEB_CLIENT_ID = '1010614156857-atfkhgj1ie7v4d08rq84dt680oregsd3.apps.googleusercontent.com';
+export const GOOGLE_IOS_CLIENT_ID = '1010614156857-8qok50fu82tnfblq0psbu53vi1fo3sgo.apps.googleusercontent.com ';
+export const GOOGLE_ANDROID_CLIENT_ID = '1010614156857-neqh3bdk68m6tlgaks0nj5khdn3n4ok4.apps.googleusercontent.com';
+export const GOOGLE_EXPO_CLIENT_ID = 'https://auth.expo.io/@your-expo-username/expense-tracker';
+
+
+
